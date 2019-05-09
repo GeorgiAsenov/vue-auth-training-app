33652ebbfbffeac4d9d90090cc572f0b38349ce8
Georgi Asenov <georgi.asenov@technofy.io>
2019-05-09 13:37:28 +0300

    test

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,17 @@
 <template>
   <div class="Login">
     <div v-if="isLoading">..Is Loading</div>
-    <div v-if="errorMessage">
-      {{ errorMessage }}
-    </div>
-<<<<<<< HEAD
-    <form @submit.prevent="submit" v-if="showForm">
-      <input type="text" v-model="form.username">
-      <input type="password" v-model="form.password">
-=======
+    <div v-if="errorMessage">{{ errorMessage }}</div>
     <form @submit.prevent="submit">
       <input type="email" v-model="form.email" placeholder="user@mail.com">
       <input type="password" v-model="form.password" placeholder="12345">
->>>>>>> c324e7407f89df5eeeba402d1df9de375d19d952
       <button type="submit">Submit</button>
     </form>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions } from 'vuex';
 
 export default {
   name: 'Login',
@@ -39,34 +31,27 @@ export default {
 
     // },
     // extract the login action from Vuex to our component to be used as a method
-    ...mapActions([
-      'login'
-    ]),
+    ...mapActions(['login']),
     submit () {
       this.isLoading = true
-      this.errorMessage = ''
+      this.errorMessage = '';
 
       // calls the login action and passes the form object to it
       this.login(this.form)
-        .then((response) => {
+        .then(response => {
           // when its done we set isLoading to false.
           this.isLoading = false
           this.showForm = false
           this.$router.push({ name: 'home' })
         })
-        .catch((error) => {
+        .catch(error => {
           // if there was an error in the API, we show it.
-<<<<<<< HEAD
           this.$notify.error({
             title: 'Error',
             text: error
           })
           this.errorMessage = error.response.data.error
           this.isLoading = false
-=======
-          this.isLoading = false
-          this.errorMessage = error.response.data
->>>>>>> c324e7407f89df5eeeba402d1df9de375d19d952
         })
     }
   }
